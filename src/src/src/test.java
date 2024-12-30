c0f38fa9f81dfbaec44584520b86c0603ff6e31a
chenxue03 <chenxue03@baidu.com>
2024-12-30 17:26:54 +0800

    Update test.java

--- src/src/src/test.java
+++ src/src/src/test.java
@@ -51,5 +51,18 @@ public class test {
 	System.out.println("Hello World!");
 	System.out.println("Hello World!");
 	System.out.println("Hello World!");
+	
+	// 这是一个冒泡排序
+	int[] arr = {1, 34, 23, 56, 78, 90};for(
+	int i = 0;i<arr.length-1;i++)
+	{
+		for (int j = 0; j < arr.length - 1 - i; j++) {
+			if (arr[j] > arr[j + 1]) {
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
 
 }
