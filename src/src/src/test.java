3aae57f35f5751bbbfeecb0a71ef18b06e556c13
chenxue03 <chenxue03@baidu.com>
2024-12-30 17:36:17 +0800

    Update test.java

--- src/src/src/test.java
+++ src/src/src/test.java
@@ -66,9 +66,34 @@ public class test {
 	}
 	
 	for(
-	int i:arr)
+	int i:arr)// 外层循环，控制冒泡排序的轮数，每轮确定一个元素的最终位置
+	for(
+	int i = 0;i<arr.length-1;i++)
+	{
+		// 内层循环，进行相邻元素的比较和交换
+		for (int j = 0; j < arr.length - 1 - i; j++) {
+			// 如果前一个元素大于后一个元素，则交换它们的位置
+			if (arr[j] > arr[j + 1]) {
+				int temp = arr[j]; // 使用临时变量temp保存arr[j]的值
+				arr[j] = arr[j + 1]; // 将arr[j+1]的值赋给arr[j]
+				arr[j + 1] = temp; // 将临时变量temp的值（原arr[j]的值）赋给arr[j+1]
+			}
+		}
+	}
 	{
 		System.out.println(i);
 	}
+	
+	// 这是一个快速排序
+	int[] arr2 = { 1, 34, 23, 56, 78, 90 };
+
+	quickSort(arr2, 0, arr2.length - 1);
+    for (int i : arr2) {
+        System.out.println(i);
+        System.out.println("Hello World!");
+        
+    }
+    
+}
 
 }
